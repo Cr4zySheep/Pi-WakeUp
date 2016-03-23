7e2bfa5e43faf19c6b2175bf0df3b63bf545c2de
Loïc M <mura.loic0@gmail.com>
2016-03-23 20:06:47 +0100

    ADD display ordered alarms list
    Create a function to ordered alarms list
    Create a specific function to calc minutes gaps between now and an alarm
    and improve it
    Hide alarms list when an alarm rings

--- timer.js
+++ timer.js
@@ -2,7 +2,6 @@ var clock; //The Flip Clock AKA the displayed clock
 var days = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday"}; //Array to convert int in string for day (Could also be a function later)
 var alarms = new Array(); //Contains all alarms of form {"day", "hours", "minutes"}
 var checkInterval; //Store the checking interval
-var nearestAlarm; //Store the next alarm which will ring (null if nothing)
 var audio; //Store html audio element (could be a class later) 
 
 //Init everything on window load
@@ -14,7 +13,7 @@ window.onload = function() {
 	audio.addEventListener('pause', function() {
 		$('audio').addClass('hide');
 		audio.load();
-		setNearestAlarm();
+		orderAlarms();
 	});
 }
 
@@ -36,19 +35,21 @@ function ringAlarm() {
 		return;
 	}
 
+	//Don't work
+	$('#alarms-planning').html('');
 	$('audio').removeClass('hide');
 	audio.play();
 }
 
 //Check if this is the time to ring
-//If yes, search the next alarm !
+//If yes, ring, order alarms and wait for the next !
 function checkAlarms() {
-	if(nearestAlarm == null) {
+	if(alarms.length < 1) {
 		return;
 	}
 
 	var date = new Date();
-	if(nearestAlarm['day'] == date.getDay() && nearestAlarm['hours'] == date.getHours() && nearestAlarm['minutes'] == date.getMinutes()) {
+	if(alarms[0]['day'] == date.getDay() && alarms[0]['hours'] == date.getHours() && alarms[0]['minutes'] == date.getMinutes()) {
 		ringAlarm();
 	}
 }
@@ -61,7 +62,7 @@ function checkSync() {
 
 	if(delay > 2) {
 		clock.setTime(time);
-		setNearestAlarm();
+		orderAlarms();
 	}
 }
 
@@ -79,79 +80,96 @@ function setAlarm(event) {
 	var alarm = {'day': day, 'hours': hours, 'minutes': minutes};
 	alarms.push(alarm);
 
-	console.log('Set alarm on : ' + days[day] + ', ' + checkNumberSize(hours) + 'h' + checkNumberSize(minutes));
+	console.log('Set an alarm on : ' + days[day] + ', ' + checkNumberSize(hours) + 'h' + checkNumberSize(minutes));
 
-	setNearestAlarm();
+	orderAlarms();
 	return false;
 }
 
-//Set th next alarm to ring
-function setNearestAlarm() {
+//Calc the minutes gap between now and the alarm
+//@now of form Date()
+//@alarm of form {"day", "hours", "minutes"}
+function calcMinutesGap(now, alarm) {
+	var day = parseInt(alarm['day']), hours = parseInt(alarm['hours']), minutes = parseInt(alarm['minutes']);
+	var currentDay = now.getDay(), currentHours = now.getHours(), currentMinutes = now.getMinutes();
+	var nDay = 0, nHours = 0, nMinutes = 0;
+
+	//We count whole day before the alarm
+	if(currentDay < day) { 			  //Case 1: the alarm is the same week
+		nDay = day - currentDay - 1; 
+	} else if(currentDay > day) {	 //Case 2: the alarm is the next week
+		nDay = 6 - currentDay + day;
+	} else if(currentDay == day) {	//Case 3: the same day, but the next week
+		if(currentHours > hours || (currentHours == hours && currentMinutes >= minutes)) {
+			nDay = 6;
+		}
+	}
+
+	//We count whole hours before the alarm (day no longer matters)
+	if(currentHours < hours) { 		  //Case 1: the alarm is the same day
+		nHours = hours - currentHours;
+		if(currentMinutes > minutes) {
+			nHours--;
+		}
+	} else if(currentHours > hours) { //Case 2: the alarm is the another day
+		nHours = 23 - currentHours + hours;
+	} else if(currentHours == hours && currentDay == day && currentMinutes >= minutes) { //Case 3: Same day but next week
+		nHours = 23;
+	}
+
+	//We count whole minutes before the alarm (day and hours no longer matters)
+	if(currentMinutes < minutes) { 		  //Case 1: the alarm is the same hour
+		nMinutes = minutes - currentMinutes - 1; 
+	} else if(currentMinutes >= minutes) { //Case 2: the alarm is the next hour
+		nMinutes = 59 - currentMinutes + minutes;
+	}
+
+	//Return the result
+	return nDay * 24 * 60 + nHours * 60 + nMinutes;
+}
+
+//Order the alarms list
+function orderAlarms() {
 	if(alarms.length < 1) {
 		return;
 	}
 
-	var today = new Date();
-	var currentDay = today.getDay(), currentHours = today.getHours(), currentMinutes = today.getMinutes();
-	var minutesArray = {};
+	var now = new Date();
+	var minutesArray = new Array();
 
 	//Calc in how many minutes each alarms will ring
 	for(var i = 0; i < alarms.length; i++) {
-		var alarm = alarms[i];
-		var day = parseInt(alarm['day']), hours = parseInt(alarm['hours']), minutes = parseInt(alarm['minutes']);
-		var nDay = 0, nHours = 0, nMinutes = 0;
-
-		//We count whole day before the alarm
-		if(currentDay < day) { 			  //Case 1: the alarm is the same week
-			nDay = day - currentDay - 1; 
-		} else if(currentDay > day) {	 //Case 2: the alarm is the next week
-			nDay = 6 - currentDay + day;
-		} else if(currentDay == day) {	//Case 3: the same day, but the next week
-			if(currentHours > hours || (currentHours == hours && currentMinutes > minutes)) {
-				nDay = 6;
-			}
-		}
-
-		//We count whole hours before the alarm (day no longer matters)
-		if(currentHours < hours) { 		  //Case 1: the alarm is the same day
-			nHours = hours - currentHours;
-			if(currentMinutes > minutes) {
-				nHours--;
-			}
-		} else if(currentHours > hours) { //Case 2: the alarm is the another day
-			nHours = 23 - currentHours + hours;
-		} else if(currentHours == hours && currentDay == day && currentMinutes > minutes) {
-			nHours = 23;
-		}
-
-		//We count whole minutes before the alarm (day and hours no longer matters)
-		if(currentMinutes < minutes) { 		  //Case 1: the alarm is the same hour
-			nMinutes = minutes - currentMinutes - 1; 
-		} else if(currentMinutes > minutes) { //Case 2: the alarm is the next hour
-			nMinutes = 59 - currentMinutes + minutes;
-		}
-
-		//Let's put this in the array
-		minutesArray[i] = nDay * 24 * 60 + nHours * 60 + nMinutes;
+		minutesArray[i] = [calcMinutesGap(now, alarms[i]), i]; //Store minutes count and index in the alarms array
 	}
 
-	//Search alarm with the lowest minutes score
-	var minIndex = -1;
+	//Order the array from smallest to biggest minutes count !
+	minutesArray.sort(function(a, b) {
+		return a[0] - b[0];
+	});
+
+	//Order the alarms list
+	var alarmsOrdered = new Array();
 	for(var i = 0; i < alarms.length; i++) {
-		if(minIndex < 0 || minutesArray[i] < minutesArray[minIndex]) {
-			minIndex = i;
-		}
+		alarmsOrdered.push(alarms[minutesArray[i][1]]);
 	}
+	alarms = alarmsOrdered;
 
 	//Conclude
-	console.log('Last alarm added in ' + nDay +' days, ' + nHours + ' hours and ' + nMinutes + ' minutes.');
-	displayNextAlarm(minIndex);
-	nearestAlarm = alarms[minIndex];
+	displayAlarms();
 }
 
-//Display next alarm on the page
-function displayNextAlarm(i) {
-	$('#next-alarm')[0].textContent =  'Next alarm : ' + days[alarms[i]['day']] + ', ' + checkNumberSize(alarms[i]['hours']) + 'h' + checkNumberSize(alarms[i]['minutes']);
+//Display alarms from the nearest to the farest
+function displayAlarms() {
+	var text = '<ol id="alarms-planning">' + function(){
+		var alarmsList = '';
+		for(var i = 0; i < alarms.length; i++) {
+			alarmsList += '<li>' + days[alarms[i]['day']] + ', ' + checkNumberSize(alarms[i]['hours']) + 'h' + checkNumberSize(alarms[i]['minutes']) + '</li>';
+		}
+		return alarmsList;
+	}() + '</ol>';
+
+	$('#alarms-planning').html(text);
+
 }
 
 //Init the displayed clock
